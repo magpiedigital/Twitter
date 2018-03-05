2c285e5cfdc5e461ba7d35fa0292808c7fc0d02b
Hamish Johnson <hamish@magpiedigital.org>
2018-03-05 14:23:04 +0000

    Initial Commit

---
+++ Twitter/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Twitter
+//
+//  Created by Hamish Johnson on 2018-03-05.
+//  Copyright © 2018 Hamish Johnson. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = UINavigationController(rootViewController: HomeController(collectionViewLayout: UICollectionViewFlowLayout()))
+        
+        return true
+    }
+
+}
+
