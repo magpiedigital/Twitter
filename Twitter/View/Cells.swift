3ae43e25c8d3a4a0abca98918dfcdd99f6656397
Hamish Johnson <hamish@magpiedigital.org>
2018-03-05 15:30:15 +0000

    Setup UserCell structure

--- Twitter/View/Cells.swift
+++ Twitter/View/Cells.swift
@@ -23,18 +23,56 @@ class UserCell: DatasourceCell {
         }
     }
     
-    override func setupViews() {
-        super.setupViews()
-        backgroundColor = .yellow
-        addSubview(nameLabel)
-        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
-    }
+    let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+    }()
     
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "Hurp"
+        label.backgroundColor = .green
         return label
     }()
+    
+    let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hurp"
+        label.backgroundColor = .purple
+        return label
+    }()
+    
+    let bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .yellow
+        return textView
+    }()
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .cyan
+        return button
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(profileImage)
+        addSubview(nameLabel)
+        addSubview(userNameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
+        
+        profileImage.anchor(topAnchor, left: leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        nameLabel.anchor(topAnchor, left: profileImage.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 12, leftConstant: 8, bottomConstant: 0, rightConstant: 8, widthConstant: 0, heightConstant: 20)
+        userNameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        bioTextView.anchor(userNameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 8, rightConstant: 8, widthConstant: 0, heightConstant: 40)
+        followButton.anchor(topAnchor, left: nil, bottom: nil, right: rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
+        
+    }
+    
+    
 }
 
 class UserFooter: DatasourceCell {
